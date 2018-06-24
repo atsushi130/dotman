44249da37fbbef34288fe5db4bf496fe24f7bda9
atsushi130 <atsushi_main@outlook.jp>
2018-06-24 18:40:53 +0900

    refactoring: fetch method

--- src/repository/dotfiles_repository.rs
+++ src/repository/dotfiles_repository.rs
@@ -6,13 +6,18 @@ pub struct DotfilesRepository;
 
 impl DotfilesRepository {
 
-    pub fn fetch(&self, file: &str) {
+    pub fn fetch(&self, file: &str) -> Option<String> {
+
         let url = format!("https://raw.githubusercontent.com/atsushi130/dotfiles/master/.{}", file);
         let command = format!("curl {}", url);
-        let a = Command::new("bash")
+        let data = Command::new("bash")
             .arg("-c")
             .arg(command)
             .output()
             .expect("fetch failed").stdout;
+
+        from_utf8(&data)
+            .ok()
+            .map(|content| format!("{}", content.to_string()))
     }
 }
