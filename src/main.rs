34bf14d05a46d548463ba4ae1b9cfd8bd319aa40
atsushi130 <atsushi_main@outlook.jp>
2018-06-24 17:41:27 +0900

    implement: print arguments of install command

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@
 extern crate clap;
 use clap::{ App, Arg, SubCommand };
 
+use std::process::{ Command, Stdio };
+
 fn main() {
 
     let dotman = App::new("dotman");
@@ -13,4 +15,17 @@ fn main() {
             Arg::with_name("rc").multiple(true)
         );
 
+    let matches = dotman.subcommand(install_command).get_matches();
+    match matches.subcommand() {
+        (_install, Some(install_matches)) => {
+            if let Some(rcs) = install_matches.values_of("rc") {
+                rcs.into_iter().for_each(|rc| {
+                    print!("{}", rc.to_string());
+                })
+            } else {
+                println!("install command required arguments.");
+            }
+        },
+        _  => {}
+    }
 }
