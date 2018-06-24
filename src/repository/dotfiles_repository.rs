e973d8560092f6522ca7e63db951f1e90db0a839
atsushi130 <atsushi_main@outlook.jp>
2018-06-24 18:27:24 +0900

    implement: rc file fetch method

--- src/repository/dotfiles_repository.rs
+++ src/repository/dotfiles_repository.rs
@@ -1,2 +1,18 @@
 
+use std::process::{ Command, Stdio };
+use std::str::from_utf8;
+
 pub struct DotfilesRepository;
+
+impl DotfilesRepository {
+
+    pub fn fetch(&self, file: &str) {
+        let url = format!("https://raw.githubusercontent.com/atsushi130/dotfiles/master/.{}", file);
+        let command = format!("curl {}", url);
+        let a = Command::new("bash")
+            .arg("-c")
+            .arg(command)
+            .output()
+            .expect("fetch failed").stdout;
+    }
+}
