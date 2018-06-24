cdba80ab710713a2c40fbed8fd10039e0406164b
atsushi130 <atsushi_main@outlook.jp>
2018-06-24 18:00:58 +0900

    implement: execute method

--- src/commands/install.rs
+++ src/commands/install.rs
@@ -1,6 +1,9 @@
 
 
-use super::super::clap::{ App, Arg, SubCommand };
+use super::super::clap::{ App, Arg, SubCommand, ArgMatches };
+use std::process::{ Command, Stdio };
+use std::str::from_utf8;
+
 pub struct Install;
 
 impl Install {
@@ -11,5 +14,20 @@ impl Install {
             .arg(
                 Arg::with_name("rc").multiple(true)
             )
+    }
+
+    pub fn execute(&self, matches: &ArgMatches) {
+        if let Some(rcs) = matches.values_of("rc") {
+                rcs.into_iter().for_each(|rc| {
+                    let a = Command::new("bash")
+                        .arg("-c")
+                        .arg(format!("echo {}", rc.to_string()))
+                        .output()
+                        .expect("install failed").stdout;
+                    print!("{}", from_utf8(&a).unwrap());
+                })
+            } else {
+                println!("install command required arguments.");
+            }
     }
 }
