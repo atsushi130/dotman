660aa52fbb1af4b562b3e9e089390f38c827a2a0
atsushi130 <atsushi_main@outlook.jp>
2018-06-24 18:02:39 +0900

    fix: indent

--- src/commands/install.rs
+++ src/commands/install.rs
@@ -18,16 +18,16 @@ impl Install {
 
     pub fn execute(&self, matches: &ArgMatches) {
         if let Some(rcs) = matches.values_of("rc") {
-                rcs.into_iter().for_each(|rc| {
-                    let a = Command::new("bash")
-                        .arg("-c")
-                        .arg(format!("echo {}", rc.to_string()))
-                        .output()
-                        .expect("install failed").stdout;
-                    print!("{}", from_utf8(&a).unwrap());
-                })
-            } else {
-                println!("install command required arguments.");
-            }
+            rcs.into_iter().for_each(|rc| {
+                let a = Command::new("bash")
+                    .arg("-c")
+                    .arg(format!("echo {}", rc.to_string()))
+                    .output()
+                    .expect("install failed").stdout;
+                print!("{}", from_utf8(&a).unwrap());
+            })
+        } else {
+            println!("install command required arguments.");
+        }
     }
 }
