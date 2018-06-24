6fd5dc4d7af09f92ed91e6cce754417c1f96b450
atsushi130 <atsushi_main@outlook.jp>
2018-06-25 00:08:17 +0900

    implement: settings repository

--- src/settings/settings_repository.rs
+++ src/settings/settings_repository.rs
@@ -1,2 +1,22 @@
 
+use super::Settings;
+use rustc_serialize::json;
+use std::fs::File;
+use std::io::Read;
+
 pub struct SettingsRepository;
+
+impl SettingsRepository {
+
+    pub fn load(&self) -> Settings {
+
+        let dir = env!("CARGO_MANIFEST_DIR");
+        let path = format!("{}/settings.json", dir);
+        let mut file = File::open(path).unwrap();
+        let mut data = String::new();
+        file.read_to_string(&mut data).unwrap();
+
+        let settings: Settings = json::decode(&data).unwrap();
+        settings
+    }
+}
