4223331a9390f149760d96fbb44650ccad6c3e96
atsushi130 <atsushi_main@outlook.jp>
2018-06-24 18:08:24 +0900

    update: Install usage

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,22 @@
 
 extern crate clap;
-use clap::{ App, Arg, SubCommand };
+use clap::App;
 
 use std::process::{ Command, Stdio };
+
+mod commands;
+use commands::Install;
 
 fn main() {
 
     let dotman = App::new("dotman");
 
-    let install = "install";
-    let install_command = SubCommand::with_name(install)
-        .about("Install specified dotfiles from your repository.")
-        .arg(
-            Arg::with_name("rc").multiple(true)
-        );
-
+    let install = Install::from();
+    let install_command = install.command();
     let matches = dotman.subcommand(install_command).get_matches();
     match matches.subcommand() {
-        (_install, Some(install_matches)) => {
-            if let Some(rcs) = install_matches.values_of("rc") {
-                rcs.into_iter().for_each(|rc| {
-                    print!("{}", rc.to_string());
-                    let a = Command::new("bash")
-                        .arg("-c")
-                        .arg(format!("echo {}", rc.to_string()))
-                        .output()
-                        .expect("install failed").stdout;
-                    print!("{}", std::str::from_utf8(&a).unwrap());
-                })
-            } else {
-                println!("install command required arguments.");
-            }
+        (Install::NAME, Some(install_matches)) => {
+            install.execute(install_matches);
         },
         _  => {}
     }
